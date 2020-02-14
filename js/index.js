0fed26133806f02387576d459a5379d10488a93d
NgugiMuthoni <ngugimuthoni43@gmail.com>
2020-02-14 16:23:18 +0300

    adding max value to html and cleaning up unused lines of code in js

--- js/index.js
+++ js/index.js
@@ -1,54 +1,45 @@
 function akanNames() {
-var day = document.getElementById("day").value;
-var DD = parseInt(day); // dd -> day
-var month = document.getElementById("month").value;
-var MM = parseInt(month); // mm -> month
-var year = document.getElementById("year").value;
-var YY = parseInt(year); // yy -> year
-var CC = parseInt(YY - 1) / 100 + 1; // cc -> century
-var dayOfTheWeek = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7; //day of the week
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-var gender;
+  var day = document.getElementById("day").value;
+  var DD = parseInt(day); // dd -> day
+  var month = document.getElementById("month").value;
+  var MM = parseInt(month); // mm -> month
+  var year = document.getElementById("year").value;
+  var YY = parseInt(year); // yy -> year
+  var CC = parseInt(YY - 1) / 100 + 1; // cc -> century
+  var dayOfTheWeek = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7; //day of the week
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var gender;
 
 
 
   if (document.getElementById("gender").checked) {
     gender = 'male';
- } else {
+  } else {
     gender = 'female';
- }
+  }
 
 
- // date val
- if (MM < 0 || MM > 12) {
-   alert("invalid month ");
- }
- // checking if day is correct and does not exceed limit.  if its not correct alert user
- else if (DD < 0 || DD > 31) {
-   alert("invalid  Date");
- }
- // validating february month ...leap year to be checked
- else if (MM == 2 && DD > 29) {
-   alert("This month does not have those number of days");
- }
- // validating year
- else if (YY < 1000 || YY > 2020) {
-   alert("Invalid year");
- }
-
- console.log('here is ',document.getElementById("gender").checked);
- console.log('and huyu ni', gender);
- console.log(typeof(gender));
- console.log('here is our case', dayOfTheWeek); 
- 
- 
-
-
+  // date val
+  if (MM < 0 || MM > 12) {
+    alert("invalid month ");
+  }
+  // checking if day is correct and does not exceed limit.  if its not correct alert user
+  else if (DD < 0 || DD > 31) {
+    alert("invalid  Date");
+  }
+  // validating february month ...leap year to be checked
+  else if (MM == 2 && DD > 29) {
+    alert("This month does not have those number of days");
+  }
+  // validating year
+  else if (YY < 1000 || YY > 2020) {
+    alert("Invalid year");
+  }
   //gender
-   if (document.getElementById("gender").checked){
+  if (document.getElementById("gender").checked) {
     //  debugger; 
-     switch (dayOfTheWeek) {
+    switch (dayOfTheWeek) {
       case 0:
         document.getElementById("results").innerHTML =
           "Your day name is " + maleNames[0] + " ,which means you were born on a Sunday.";
@@ -76,7 +67,7 @@ var gender;
       case 6:
         document.getElementById("results").innerHTML =
           "Your day name is " + maleNames[6] + " ,which means you were born on a Saturday.";
-        break; 
+        break;
     }
   } else {
     switch (dayOfTheWeek) {
